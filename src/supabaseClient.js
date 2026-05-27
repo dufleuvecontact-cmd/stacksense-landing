8c641f7a6964eb133f48e615b3422cfadd92b6d2
Jad G <dufleuvecontact@gmail.com>
2026-05-27 19:10:44 -0400

    chore: update supabase project keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://pjapdxatvghedkbxtnzo.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_D-78iY0Zo31g-0ZU684RIA_OtzTPiUr'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://thbuuvbfyxblyuydpcic.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoYnV1dmJmeXhibHl1eWRwY2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMjE1MTAsImV4cCI6MjA5MzU5NzUxMH0.5lvARO0DFyflVetpm3CjVaFsAspQkoGwZi82B6_85Kw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
